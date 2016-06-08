6d17dabf9bba4dee1271a350f8849e222ea73e12
abdulrahmanelfeky <abdulrahman.elfeky@gmail.com>
2016-06-08 23:44:42 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,9 @@
 <?php
 
 // see README.md for an explanation of these config params
+
 $TWILIO_ACCOUNT_SID = 'ACf90bc0dfd2fbf6e6d50cc04832c1cc4e';
 $TWILIO_IPM_SERVICE_SID = 'IS8e9b413b7b6d49cf9547c7bc57f9e542';
-$TWILIO_API_KEY = 'SKe71f0b9d0ff39cbeed44d75410117ad0';
-$TWILIO_API_SECRET = 'g2RvR2SkVl9KL7cuzB85r08mkukGi6no';
+$TWILIO_API_KEY = 'SK5d9bf38248fc94820ab79d8385489269';
+$TWILIO_API_SECRET = 'xUTSyreOSInRrZtLC6HftdY3veZZH0VM';
+?>
